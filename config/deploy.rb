bf8f28fb76501761d4761988c438e14a35489ca1
Monique Rio <nique.rio@gmail.com>
2016-12-13 18:46:23 -0500

    added Gemfile.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log')
