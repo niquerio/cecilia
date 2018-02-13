77d23c2cc91d42a055d545d507e821ec18704f49
Monique Rio <nique.rio@gmail.com>
2018-02-12 21:30:59 -0500

    changed production deploy to use my caroso account

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{aelkiss@caroso.elkiss.com}
-role :web, %w{aelkiss@caroso.elkiss.com}
-role :db,  %w{aelkiss@caroso.elkiss.com}
+role :app, %w{mrio@caroso.elkiss.com}
+role :web, %w{mrio@caroso.elkiss.com}
+role :db,  %w{mrio@caroso.elkiss.com}
 
 
 
